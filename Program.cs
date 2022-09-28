ad7c969fb65161ab64f73bb80469efdfc11d5bfe
«olga» <lilu_009@mail.ru>
2022-09-29 01:53:41 +0800

    program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = { "Hello", "2", "world", ":-)", "1234", "1567", "-2", "Russia", "Denmark", "Kazan" };
+string[] newArray = new string[array.Length];
+
+void NewArray(string[] array, string[] newArray)
+{
+    int j = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write(newArray[j] = array[i] + " ");
+            j++;
+        }
+    }
+}
+
+NewArray(array, newArray);
